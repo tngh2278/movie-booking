67cd5399ca213bb735b6cf9c97242e5599ff8692
tngh2278 <tngh2278@gmail.com>
2026-09-21 11:31:32 +0900

    없는 영화 조회 시 404 오류 응답

--- src/main/java/com/moviebooking/movie_booking/Movie/MovieService.java
+++ src/main/java/com/moviebooking/movie_booking/Movie/MovieService.java
@@ -2,6 +2,8 @@ package com.moviebooking.movie_booking.Movie;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MovieService {
@@ -17,6 +19,9 @@ public class MovieService {
 
     public Movie getMovie(Long id) {
         return movieRepository.findById(id)
-                .orElseThrow(); // 영화가 존재하지 않을 시 오류
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "영화를 찾을 수 없습니다." // 영화가 존재하지 않을 시 오류
+                ));
     }
 }
